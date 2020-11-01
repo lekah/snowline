ba0ab639715fba4d844bb13dc5e31ecfe686c5f7
leo@pegasus <leonid.kahle@epfl.ch>
2020-11-01 21:39:50 +0100

    Added utilities for input-output and analysis module for analysis of snowmaps

---
+++ snowline/analysis/snowmap.py
@@ -0,0 +1,117 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.ndimage import measurements
+
+
+
+class SnowMap(object):
+    def __init__(self, array):
+        """
+        :param array: the original array, containing values of 0 for no snow, 1 for snow,
+        and -1 for unknown.
+        """
+        if type(array).__module__ != np.__name__:
+            return TypeError("array passed has to be numpy array")
+        if array.dtype != 'int64':
+            return TypeError("array passed has to be an integer array")
+        if set(np.unique(array)).difference(range(-1,2)):
+            raise ValueError("Array can only containv values -1, 0, 1")
+        self._array = array.copy()
+
+    def get_array(self):
+        return self._array.copy()
+
+    def filter_size_nonsnow(self, limit_nonsnow_patch_size):
+        """
+        Cleans up snowmap by first finding patches (=clusters) of non-snow within snow and, second,
+        getting rid of small non-snow clusters below the threshold size.
+        :param int limit_nonsnow_patch_size: the threshold below which clusters of non-snow are
+        removed.
+        """
+
+        if limit_nonsnow_patch_size < 1:
+            return
+    
+        # Use scipy measurements.label to find clusters.
+        # structure tells it which neighborhood kind to apply.
+        # For now Neumann, but maybe this can be an input #TODO
+        nonsnow_clusters, num_clusters = measurements.label(self._array==0, 
+                                structure=[[0,1,0], [1,1,1], [0,1,0]])
+        cluster_indices, counts = np.unique(nonsnow_clusters, return_counts=True)
+        msk_nonzero = cluster_indices != 0 # cluster 0 contains no snow, no need to include in count
+
+        # remove all cluster that are smaller than limit
+        small_cluster_indices = cluster_indices[counts < limit_nonsnow_patch_size]
+        msk = np.isin(nonsnow_clusters, small_cluster_indices)
+        # msk is now set to True for all clusters that are smmaller limit_snow
+        self._array[msk] = 1
+
+    def filter_size_snow(self, limit_snow_patch_size):
+        """
+        Cleans up snowmap by finding patches (=clusters) of snow and
+        removing clusters below the threshold size.
+        :param int limit_snow_patch_size: the threshold cluster size
+        """
+        if limit_snow_patch_size < 1:
+            return
+        # Use scipy measurements.label to find clusters.
+        # structure tells it which neighborhood kind to apply.
+        # For now Neumann, but maybe this can be an input #TODO
+        snow_clusters, num_clusters = measurements.label(self._array==1, 
+                        structure=[[0,1,0], [1,1,1], [0,1,0]])
+        cluster_indices, counts = np.unique(snow_clusters, return_counts=True)
+        msk_nonzero = cluster_indices != 0 # cluster 0 contains no snow, no need to include in count
+        small_cluster_indices = cluster_indices[counts < limit_snow_patch_size]
+        msk = np.isin(snow_clusters, small_cluster_indices)
+        self._array[msk] = 0
+
+    def get_num_clusters(self):
+        return measurements.label(self._array==1, structure=[[0,1,0], [1,1,1], [0,1,0]])[1]
+
+    def get_boundaries(self):
+        """
+        Get the points around the snow patches
+        """
+        
+        snow_clusters, num_clusters = measurements.label(self._array==1,
+                            structure=[[0,1,0], [1,1,1], [0,1,0]])
+        cluster_indices, counts = np.unique(snow_clusters, return_counts=True)
+        for cluster_index in cluster_indices:
+            cs  = plt.contour(snow_clusters==cluster_index, levels=(0.5,))
+            yield cs.allsegs[0]
+        # Legacy code to make the boundary, which might be useful later
+        # ~ boundary_msk = get_boundary_msk(snow_clusters, 1)
+        # ~ R = np.array([[0,-1],[1,0]]) # Rotation matrix for 90 degress
+
+        # ~ points_all = []
+
+        # ~ Npixels = 2*boundary_msk.sum() # every pixel can be visited 2, in theory
+        # ~ remainder = boundary_msk.copy()
+        # ~ count = 0
+        # ~ i, j = get_leftmost(remainder)
+        # ~ start_i, start_j = i, j
+        # ~ v = np.array([1,0])
+        # ~ points = []
+        # ~ while True:
+            # ~ v = -v # rotating v around 180
+            # ~ for t in range(4):  # applying 4 rotation (leftwise, consecutively)
+                # ~ v = R.dot(v)
+                # ~ if boundary_msk[i+v[0], j+v[1]]: # if I find a boundary pixel, I go there
+                    # ~ break
+                # ~ else:
+                    # ~ # If I don't find a boundary pixel, this is the boundary.
+                    # ~ # add a point.
+                    # ~ points.append([j+0.5*v[1], i+0.5*v[0]])
+                    # ~ #points.append([ i+0.5*v[0], j+0.5*v[1]])
+            # ~ last_i, last_j = i, j
+            # ~ remainder[i, j] = False
+            # ~ i+=v[0]
+            # ~ j+=v[1]
+            # ~ count+=1
+            # ~ if (i==start_i and j==start_j):
+                # ~ # close the loop
+                # ~ points.append(points[0])
+                # ~ break
+            # ~ if count > Npixels:
+                # ~ raise RuntimeError("Number of iterations exceeded number of boundary pixels")
+
---
+++ snowline/utils/io.py
@@ -0,0 +1,52 @@
+import numpy as np
+import os
+from PIL import Image
+
+def load_array(filename):
+    """
+    Given a valid image, load the image and return the pixels as a numpy array
+    :param filename: The filename as a string
+    :returns: A numpy array which stores the pixel data from a snowmap
+
+    Convention is as follows: pixels that read 0,0,0, 255 are read as snow-free and contain the value 0;
+    pixels that read 0,0,0,0 assume no data and return -1, and pixels that read (255, 255, 255, 255)
+    are read as snow and get the value 1
+    """
+
+    image = Image.open(filename)
+    image.load()
+    height, width = image.size
+
+    snowmap = np.zeros((height, width), dtype=int)
+    for row in range(height):
+        for col in range(width):
+            a = image.getpixel((row,col))
+            if a == (0, 0, 0, 255): # This is no snow
+                snowmap[row, col] = 0
+            elif a ==  (0, 0, 0, 0): # this is no data
+                snowmap[row, col] = -1
+            elif a ==  (255, 255, 255, 255): # that's for snow
+                snowmap[row, col] = 1
+            else:
+                raise ValueError("Unknown Pixel value {}".format(a))
+    return snowmap
+
+
+def get_coordinates(filename):
+    """
+    Get coordinates from tiff file using gdal
+    Assume they are printed something like this:
+     Upper Left  (   5.8000000,  47.8900000) (  5d48' 0.00"E, 47d53'24.00"N)
+     Lower Left  (   5.8000000,  45.6000000) (  5d48' 0.00"E, 45d36' 0.00"N)
+     Upper Right (  12.7318760,  47.8900000) ( 12d43'54.75"E, 47d53'24.00"N)
+     Lower Right (  12.7318760,  45.6000000) ( 12d43'54.75"E, 45d36' 0.00"N)
+    """
+    metadata_text = os.popen('gdalinfo {}'.format(filename)).readlines()
+    for iline, line in enumerate(metadata_text):
+        if line.strip() == 'Corner Coordinates:':
+            break
+    coords = []
+    for iline in range(iline+1, iline+5):
+        coords.append(list(map(float, "".join(
+                metadata_text[iline].split()[3:5]).lstrip('(').rstrip(')').split(','))))
+    return np.array(coords)

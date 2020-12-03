e4abb5164e6931c7db86b0f401cf99a7b264a103
leo@pegasus <leonid.kahle@epfl.ch>
2020-12-03 19:14:08 +0100

    Minor change to path in test

--- tests/test_snowline.py
+++ tests/test_snowline.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import os
 from scipy.ndimage import measurements
 
 from snowline.analysis.snowmap import SnowMap, UpdatedSnowMap
@@ -7,7 +8,11 @@ from snowline.analysis.grid import Grid
 
 class TestSnowmap(unittest.TestCase):
     def test_read_snowline(self):
-        filename='/home/leo/temp/snowline-data/L2SNOW_reproj_idepix_subset_S3A_OL_1_EFR____20180102T100908_20180102T101208_20180103T152552_0179_026_179_2160_LN1_O_NT_002.SEN3.nc'
+        filename = os.path.join(
+                os.path.abspath(os.path.dirname(__file__)), 
+                'data',
+                'L2SNOW_reproj_idepix_subset_S3A_OL_1_EFR____20180102T100908_20180102T101208_20180103T152552_0179_026_179_2160_LN1_O_NT_002.SEN3.nc'
+            )
         snowmap_ntf = SnowMap.from_netcdf(filename, transform=False)
         self.assertFalse(snowmap_ntf._is_internal)
 
